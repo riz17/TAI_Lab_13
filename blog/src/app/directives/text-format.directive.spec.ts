89673fdd3d1ba71dc1d378f5ba73ee9a8d076fa0
Kamil Adamczyk <kamiladamczyk3k@gmail.com>
2020-04-07 15:42:14 +0200

    v3

--- blog/src/app/directives/text-format.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TextFormatDirective } from './text-format.directive';
-
-describe('TextFormatDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TextFormatDirective();
-    expect(directive).toBeTruthy();
-  });
-});
